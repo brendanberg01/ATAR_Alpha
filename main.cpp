abb5a27432026a0b800696ff15dbcdf621d77042
Brendan Berg <brendan_berg@aol.com>
2019-04-22 23:48:57 +0200

    Add UART interrupt slave com and accelerometer
    The UART component of the AVR_Lib was extended with a class for
    slave-side interrupt driven UART communication. A send request is
    triggered by an external interrupt. In that case, a UARTDataSource is
    used to collect data to be transmitted to the master. UARTDataSource can
    also be used for other UART communication patterns.
    
    The Accelerometer class was completed. It was extended with an
    auto-calibrate function. Also, angle conversion was added.

--- main.cpp
+++ main.cpp
@@ -4,43 +4,90 @@
 #include "mcu/io/AnalogInputController.hpp"
 #include "mcu/io/AnalogInputPin.hpp"
 
+#include "mcu/usart/UARTDataSource.hpp"
 #include "mcu/usart/UARTConnection.hpp"
+#include "mcu/usart/UARTInterruptSlaveConnection.hpp"
 
 #include "periph/LightEmittingDiode.hpp"
-#include "periph/Acceleromter.hpp"
+#include "periph/Accelerometer.hpp"
+
+
+class SensorData : public UARTDataSource
+{
+public:
+
+    explicit SensorData (Accelerometer& accelerometer)
+        : UARTDataSource(16), m_Accelerometer(accelerometer)
+    { }
+
+
+protected:
+
+    void UpdateMessage () override
+    {
+        auto accelX = m_Accelerometer.GetAcceleration(0);
+        auto accelZ = m_Accelerometer.GetAcceleration(1);
+        auto angle = m_Accelerometer.GetClimbAngle(1, 0);
+        m_Message[0] = accelX >> 8;
+        m_Message[1] = accelX & 0xff;
+        m_Message[2] = accelZ >> 8;
+        m_Message[3] = accelZ & 0xff;
+        m_Message[4] = angle;
+    }
+
+
+protected:
+
+    Accelerometer& m_Accelerometer;
+
+
+};
 
 
 int main ()
 {
-    // TODO: interrupt handler
-    UARTConnection uartCentral(0, 38400);
-
     SingleColorLED ledHeartbeat;
     ledHeartbeat.RegisterPin(&PORTB, PB5);
 
     AnalogInputController analogInputController;
 
-    // TODO: outsource
-    AnalogInputPin sharpRF(0x01, 4);
-    analogInputController.RegisterAnalogPin(&sharpRF);
-    AnalogInputPin sharpRB(0x02, 4);
-    analogInputController.RegisterAnalogPin(&sharpRB);
-    AnalogInputPin sharpLB(0x03, 4);
-    analogInputController.RegisterAnalogPin(&sharpLB);
-    AnalogInputPin sharpLF(0x00, 4);
-    analogInputController.RegisterAnalogPin(&sharpLF);
+//    // TODO: outsource
+//    AnalogInputPin sharpRF(0x01, 4);
+//    analogInputController.RegisterAnalogPin(&sharpRF);
+//    AnalogInputPin sharpRB(0x02, 4);
+//    analogInputController.RegisterAnalogPin(&sharpRB);
+//    AnalogInputPin sharpLB(0x03, 4);
+//    analogInputController.RegisterAnalogPin(&sharpLB);
+//    AnalogInputPin sharpLF(0x00, 4);
+//    analogInputController.RegisterAnalogPin(&sharpLF);
 
-    Acceleromter acceleromter(0x04, 0xff, 0x05, analogInputController);
+    Accelerometer accelerometer(analogInputController);
+    accelerometer.RegisterAxis(0x04, 0x00);
+    accelerometer.RegisterAxis(0x05, 0xff);
 
     analogInputController.Enable();
 
+    SensorData sensorData(accelerometer);
+    UARTInterruptSlaveConnection uartCentral(0x00, 38400, 0x00, sensorData);
+
     sei();
 
+    _delay_ms(1000);
+
+    accelerometer.Calibrate();
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
     while (true)
-    { }
+    {
+        while (uartCentral.DataAvailable())
+        {
+            if (uartCentral.ReadByte() == '1')
+            {
+                ledHeartbeat.Flip();
+            }
+        }
+    }
 #pragma clang diagnostic pop
 
 
